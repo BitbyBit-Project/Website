2e47560ce60fc488eb3b384cd48807f63ff7888b
mm-978 <110142347+mm-978@users.noreply.github.com>
2023-05-08 17:38:56 -0700

    Tech Stack Update
    -Removed tech stack from artifacts page and moved it on to technology page
    -Reformatted tech stack layout

--- src/pages/artifacts-page.js
+++ src/pages/artifacts-page.js
@@ -63,7 +63,7 @@ export const ArtifactsPage = () => {
         </div>
 
         <p>
-          <br></br>
+          
           <hr></hr>
           <br></br>
         </p>
@@ -83,108 +83,6 @@ export const ArtifactsPage = () => {
           <hr></hr>
           <br></br>
         </p>
-
-        <div className="content__body">
-          <p id="page-description">
-            <span>
-             Nursing Virtual Reality powered by: 
-            </span>
-          </p>
-        </div>
-
-       
-
-      <div class="flex-parent-element">
-        <div class="flex-child-element magenta">Flex Column 1</div>
-        <div class="flex-child-element green">Flex Column 2</div>
-      </div>
-
-    <div class = "column" align="center">
-              <p>
-                <img src = {"https://i.imgur.com/0PxXsyO.png"} width = "60" height = "40" alt = "Meta" />
-                <span> Meta Quest Developer Hub</span>
-              </p>
-
-              <p>
-                <img src = {"https://i.imgur.com/aXnDfLc.png"} width = "40" height = "40" alt = "Unity" />
-                <span> Unity</span>
-              </p>
-
-              <p>
-                <img src = {"https://i.imgur.com/hVyuGT6.png"} width = "40" height = "40" alt = "VSCode" />
-                <span> Visual Studio Code</span>
-              </p>
-
-              <p>
-                <img src = {"https://i.imgur.com/ZBE9YDe.png"} width = "50" height = "50" alt = "React" />
-                <span> React</span>
-              </p>
-
-              <p>
-                <img src = {"https://i.imgur.com/zQC1wUg.png"} width = "75" height = "50" alt = "AWS" />
-                <span> Amazon Web Services</span>
-              </p>
-
-              <p>
-                <img src = {"https://i.imgur.com/HJdzJWT.png"} width = "40" height = "40" alt = "Maya" />
-                <span> Autodesk Maya</span>
-              </p> 
-
-              <p>
-                <img src = {"https://i.imgur.com/BR1xOjZ.png"} width = "75" height = "50" alt = "MySQL" />
-                <span> MySQL</span>
-              </p>
-
-              <p>
-                <img src = {"https://i.imgur.com/Fg5M6LF.png"} width = "50" height = "50" alt = "Adobe Audition" />
-                <span> Adobe Audition</span>
-              </p> 
-
-              <p>  
-                <img src = {"https://i.imgur.com/6EWSJ38.pngg"} width = "50" height = "50" alt = "Adobe Mixamo" />
-                <span> Adobe Mixamo</span>
-              </p>
-
-              <p>
-                <img src = {"https://i.imgur.com/bL7y7oO.png"} width = "50" height = "50" alt = "GitHub" /> 
-                <span> Github</span>
-              </p> 
-
-              <p>  
-                <img src = {"https://i.imgur.com/QwquTqZ.png"} width = "50" height = "50" alt = "Jira" />
-                <span> Jira</span>
-              </p>
-    </div>
-        
-            
-
-    <div className="content__body">
-          <p id="page-description">
-            <span>
-             With languages: 
-            </span>
-          </p>
-    </div>
-
-
-    <p align="center">
-        <div class="column">
-          <img src = {"https://i.imgur.com/8SCfipV.png"} width = "50" height = "50" alt = "C#" />
-          <span>C#</span>
-
-          <img src = {"https://i.imgur.com/ZOo1OTl.png"} width = "50" height = "50" alt = "C#" />
-          <span>CSS</span>
-
-          <img src = {"https://i.imgur.com/HHdwP9v.png"} width = "50" height = "50" alt = "C#" />
-          <span>JavaScript</span>
-
-          <img src = {"https://i.imgur.com/Lgbw1Fr.png"} width = "50" height = "50" alt = "C#" />
-          <span>HTML</span>
-        
-          <img src = {"https://i.imgur.com/8h6natt.png"} width = "75" height = "50" alt = "C#" />
-          <span>PHP</span>
-        </div>
-    </p>
 
       </div>
     </PageLayout>
--- src/pages/tech-stack.js
+++ src/pages/tech-stack.js
@@ -15,6 +15,13 @@ export const TechPage = () => {
         <h1 id="page-title" className="content__title">
           Technology Stack
         </h1>
+
+        <p>
+          <br></br>
+          <hr></hr>
+          <br></br>
+        </p>
+
         <div className="content__body">
           <p id="page-description">
             <span>
@@ -28,6 +35,146 @@ export const TechPage = () => {
           <hr></hr>
           <br></br>
         </p>
+
+        
+        <div className="content__body">
+          <p id="page-description">
+            <span>
+             
+            </span>
+          </p>
+        </div>
+
+        <p>
+          <br></br>
+          <br></br>
+        </p>
+       
+
+        <div class = "column" align="center">
+      <div class="flex-parent-element">
+        
+        <div class="flex-child-element magenta">
+          <img src = {"https://i.imgur.com/0PxXsyO.png"} width = "60" height = "40" alt = "Meta" />
+          <p><span> Meta Quest Developer Hub</span></p>
+        </div>
+
+        <div class="flex-child-element magenta">
+          <img src = {"https://i.imgur.com/aXnDfLc.png"} width = "45" height = "45" alt = "Unity" />
+          <p> <span> Unity</span></p>
+        </div>
+
+        <div class="flex-child-element magenta">
+         <img src = {"https://i.imgur.com/hVyuGT6.png"} width = "40" height = "40" alt = "VSCode" />
+         <p><span> Visual Studio Code</span></p>
+        </div>
+
+        <div class="flex-child-element magenta">
+          <img src = {"https://i.imgur.com/zQC1wUg.png"} width = "60" height = "40" alt = "AWS" />
+          <p><span> Amazon Web Services</span></p>
+        </div>
+
+        <div class="flex-child-element magenta">
+          <img src = {"https://i.imgur.com/BR1xOjZ.png"} width = "70" height = "45" alt = "MySQL" />
+         <p> <span> MySQL</span></p>
+        </div>
+
+        <div class="flex-child-element magenta">
+         <img src = {"https://i.imgur.com/ZBE9YDe.png"} width = "50" height = "50" alt = "React" />
+         <p> <span> React</span></p>
+        </div>
+        
+        </div>
+        </div>
+
+        
+    <div class = "column" align="center">
+      <div class="flex-parent-element">
+        
+        <div class="flex-child-element magenta">
+          <img src = {"https://i.imgur.com/HJdzJWT.png"} width = "48" height = "48" alt = "Maya" />
+         <p><span> Autodesk Maya</span></p>
+        </div>
+
+        <div class="flex-child-element magenta">
+          <img src = {"https://i.imgur.com/Fg5M6LF.png"} width = "50" height = "50" alt = "Adobe Audition" />
+         <p><span> Adobe Audition</span></p>
+        </div>
+
+        <div class="flex-child-element magenta">
+          <img src = {"https://i.imgur.com/6EWSJ38.pngg"} width = "50" height = "50" alt = "Adobe Mixamo" />
+         <p><span> Adobe Mixamo</span></p>
+        </div>
+
+        <div class="flex-child-element magenta">
+          <img src = {"https://i.imgur.com/bL7y7oO.png"} width = "50" height = "50" alt = "GitHub" /> 
+          <p> <span> Github</span></p>
+        </div>
+
+        <div class="flex-child-element magenta">
+          < img src = {"https://i.imgur.com/QwquTqZ.png"} width = "50" height = "50" alt = "Jira" />
+          <p><span> Jira</span></p>
+        </div>
+
+
+        </div>
+        </div>
+
+       
+        
+
+
+
+      
+
+    <div className="content__body">
+          <p id="page-description">
+            <span>
+             Featured languages: 
+            </span>
+          </p>
+    </div>
+
+
+    
+
+
+    <div class = "column" align="center">
+      <div class="flex-parent-element">
+       
+        <div class="flex-child-element magenta">
+          <img src = {"https://i.imgur.com/8SCfipV.png"} width = "50" height = "50" alt = "C#" />
+         <p> <span>C#</span></p>
+        </div>
+
+        <div class="flex-child-element magenta">
+          <img src = {"https://i.imgur.com/ZOo1OTl.png"} width = "50" height = "50" alt = "C#" />
+         <p> <span>CSS</span></p>
+        </div>
+
+        <div class="flex-child-element magenta">
+         <img src = {"https://i.imgur.com/HHdwP9v.png"} width = "50" height = "50" alt = "C#" />
+          <p><span>JavaScript</span></p>
+        </div>
+
+        <div class="flex-child-element magenta">
+          <img src = {"https://i.imgur.com/Lgbw1Fr.png"} width = "50" height = "50" alt = "C#" />
+         <p> <span>HTML</span></p>
+        </div>
+
+        <div class="flex-child-element magenta">
+        <img src = {"https://i.imgur.com/8h6natt.png"} width = "75" height = "50" alt = "C#" />
+        <p> <span>PHP</span></p>
+        </div>
+
+
+        </div>
+        </div>
+
+
+
+
+
 
         <div>
         </div>
